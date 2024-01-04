262fa98f7cdfce97ac2c09790c43f172de3751a1
Scott Semtner <sjsemtner@gmail.com>
2024-01-04 20:20:53 +0000

    js to fix nav dropdowns

--- _includes/scripts/page.js
+++ _includes/scripts/page.js
@@ -1,3 +1,9 @@
-/*(function () {
-
-})();*/
+(function () {
+  Array.prototype.forEach.call(document.getElementsByClassName("navigation__folder"), function (element) {
+    element.addEventListener("mouseover", function () {
+      const child = element.querySelector("ul");
+      const position = element.getBoundingClientRect();
+      child.style.left = position.left + "px";
+    });
+  });
+})()
